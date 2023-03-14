2265a304b6ca27ac387e3ccc51e21b0eaf4b03ca
Dritan <105552400+DritanR@users.noreply.github.com>
2023-03-14 23:45:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-document.querySelector('button').addEventListener('click', getNewJoke)
- function getNewJoke () {
-    const getValue= document.querySelector('button').value
-    const url= `https://v2.jokeapi.dev/joke/Any?safe-mode=${getValue}`
-    fetch (url)
-        .then(res => res.json ())
-        .then(data => {
-            console.log(data)
-            if (document.querySelector('#joker').innerText= data.joke) {
-                document.querySelector('#listener').innerText= (('That was funny!') || ('Hahahaha!') || ('That was funny AF !'))
-            }else if (document.querySelector('#joker').innerText= data.setup) {
-                document.querySelector('#listener').innerText= data.delivery
-            }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
- }
+//Create a button that adds 1 to a botScore stored in localStorage 
+
+
